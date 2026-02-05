1f86ac664ed99a59578745941badbefa09ee4fec
oletto <oletheodor04@gmail.com>
2026-02-05 14:42:08 +0100

    Add StockSimulator class for simulating stock price movements

---
+++ src/main/java/ntnu/idatt2003/group15/model/StockSimulator.java
@@ -0,0 +1,16 @@
+package ntnu.idatt2003.group15.model;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+public class StockSimulator {
+  private final Random random = new Random();
+
+  public BigDecimal nextPrice(double currentPrice, double drift, double volatility, double dt) {
+    double z = random.nextGaussian();
+    double exponent = (drift - 0.5 * Math.pow(volatility, 2)) * dt
+        + (volatility * Math.sqrt(dt) * z);
+    return BigDecimal.valueOf(currentPrice * Math.exp(exponent));
+  }
+
+}
