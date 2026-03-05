3fddccd447a849919d6a7cb0e3b2591f061f19bd
Fredrik Olsen <69763370+Fredneyy@users.noreply.github.com>
2026-03-05 14:06:22 +0100

    Revert "15 create junit tests for the stock and share classes"

--- src/main/java/ntnu/idatt2003/group15/model/BlankArgumentException.java
+++
@@ -1,7 +0,0 @@
-package ntnu.idatt2003.group15.model;
-
-public class BlankArgumentException extends RuntimeException {
-    public BlankArgumentException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ntnu/idatt2003/group15/model/Stock.java
+++ src/main/java/ntnu/idatt2003/group15/model/Stock.java
@@ -1,31 +1,20 @@
 package ntnu.idatt2003.group15.model;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
-import static ntnu.idatt2003.group15.utilities.InputValidator.isBigDecimalValuePositive;
 
 public class Stock {
   private final String symbol;
   private final String company;
   private List<BigDecimal> prices;
 
-  public Stock(String symbol, String company, BigDecimal salesPrice) throws NullPointerException, BlankArgumentException, IllegalArgumentException {
-      Objects.requireNonNull(symbol, "Company cannot be null");
-      if (symbol.isBlank()) {
-          throw new BlankArgumentException("Symbol cannot be blank");
-      }
+  public Stock(String symbol, String company, BigDecimal salesPrice) {
+      Objects.requireNonNull(symbol, "Symbol cannot be null");
       Objects.requireNonNull(company, "Company cannot be null");
-      if (company.isBlank()) {
-          throw new BlankArgumentException("Company cannot be blank");
-      }
-      isBigDecimalValuePositive("SalesPrice", salesPrice);
-
+      Objects.requireNonNull(salesPrice, "SalesPrice cannot be null");
     this.symbol = symbol;
     this.company = company;
-    prices = new ArrayList<BigDecimal>();
     this.prices.add(salesPrice);
   }
 
@@ -42,11 +31,7 @@ public class Stock {
     return this.prices.getLast();
   }
 
-  public void addNewSalesPrice(BigDecimal price) throws IllegalArgumentException, NullPointerException {
-      if (isBigDecimalValuePositive("SalesPrice", price)) {
-          this.prices.add(price);
-      }
+  public void addNewSalesPrice(BigDecimal price) {
+    this.prices.add(price);
   }
-
-
 }
