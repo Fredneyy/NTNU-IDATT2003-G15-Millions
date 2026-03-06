1726f27a5e28425f50512a561666902612f56db5
oletto <oletheodor04@gmail.com>
2026-03-06 10:08:35 +0100

    Add null and blank checks to Player constructor

--- src/main/java/ntnu/idatt2003/group15/model/Player.java
+++ src/main/java/ntnu/idatt2003/group15/model/Player.java
@@ -11,7 +11,7 @@ public class Player {
   private final Portfolio portfolio = new Portfolio();
   private final TransactionArchive transactionArchive = new TransactionArchive();
 
-  public Player(String name, BigDecimal startingMoney) throws BlankArgumentException, NullPointerException {'
+  public Player(String name, BigDecimal startingMoney) throws BlankArgumentException, NullPointerException {
       Objects.requireNonNull(name, "Name cannot be zero");
       if (name.isBlank()) {
           throw new BlankArgumentException("Name cannot be blank");
