69a27e9c494ffbc5a95b03cbfc9a3e1648c610f5
Ole Theodor Torkildsen <oletto@stud.ntnu.no>
2026-03-19 13:29:07 +0100

    Add CsvUtil for reading CSV files and FileReaderException for error handling

---
+++ src/main/java/ntnu/idatt2003/group15/model/FileReaderException.java
@@ -0,0 +1,15 @@
+package ntnu.idatt2003.group15.model;
+
+public class FileReaderException extends RuntimeException {
+
+  private final Throwable cause;
+
+  public FileReaderException(String message, Throwable cause) {
+    super(message);
+    this.cause = cause;
+  }
+
+  public Throwable getCause() {
+    return cause;
+  }
+}
