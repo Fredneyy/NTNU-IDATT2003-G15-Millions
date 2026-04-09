62f7cd3c9a18fbe43f006848a271243c2cf318cb
Ole Theodor Torkildsen <oletto@stud.ntnu.no>
2026-04-09 14:05:40 +0200

    Refactor App class to extend Application; implement JavaFX start method for UI initialization

--- src/main/java/ntnu/idatt2003/group15/App.java
+++ src/main/java/ntnu/idatt2003/group15/App.java
@@ -1,12 +1,20 @@
 package ntnu.idatt2003.group15;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * The main entry point for the Millions stock simulation application.
  */
-public class App 
-{
-    void main()
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        StackPane root =  new StackPane();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
