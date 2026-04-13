0ec688f7e1d7eba788bcc99b0c9030810a749029
Ole Theodor Torkildsen <oletto@stud.ntnu.no>
2026-04-13 12:36:27 +0200

    Add PortfolioController and TransactionArchiveController for managing portfolio and transaction data

---
+++ src/main/java/ntnu/idatt2003/group15/controller/PortfolioController.java
@@ -0,0 +1,45 @@
+package ntnu.idatt2003.group15.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import javafx.collections.ObservableList;
+import ntnu.idatt2003.group15.model.Portfolio;
+import ntnu.idatt2003.group15.model.Share;
+
+public class PortfolioController {
+
+  private final Portfolio portfolio;
+
+  public PortfolioController(Portfolio portfolio) {
+    this.portfolio = Objects.requireNonNull(portfolio, "Portfolio cannot be null");
+  }
+
+  public boolean addShare(Share inputShare) {
+    return portfolio.addShare(inputShare);
+  }
+
+  public boolean removeShare(Share inputShare) {
+    return portfolio.removeShare(inputShare);
+  }
+
+  public ObservableList<Share> getListProperty() {
+    return portfolio.getListProperty();
+  }
+
+  public List<Share> getShares() {
+    return portfolio.getShares();
+  }
+
+  public List<Share> getShares(String symbol) {
+    return portfolio.getShares(symbol);
+  }
+
+  public boolean contains(Share inputShare) {
+    return portfolio.contains(inputShare);
+  }
+
+  public BigDecimal getTotalMarketValue() {
+    return portfolio.getTotalMarketValue();
+  }
+}
