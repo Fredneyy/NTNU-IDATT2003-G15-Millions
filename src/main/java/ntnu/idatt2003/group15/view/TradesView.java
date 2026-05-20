f8f90f0d17f942a4ab76ad73ba5be51ba38badd2
Ole Theodor Torkildsen <oletto@stud.ntnu.no>
2026-05-20 09:39:43 +0200

    Refactor MarketTableView and TradesView for improved clarity; remove unnecessary comments and enhance layout properties

--- src/main/java/ntnu/idatt2003/group15/view/TradesView.java
+++ src/main/java/ntnu/idatt2003/group15/view/TradesView.java
@@ -16,7 +16,6 @@ import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
